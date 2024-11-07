1e2be50e93f1d3e19898a15ac7972224edb5beaf
bstteam <123465459+bstte@users.noreply.github.com>
2024-11-07 15:33:29 +0530

    Update Login.js

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -23,6 +23,12 @@ const Login = () => {
 
   return (
     <div className="login-container">
+      {/* Add the logo image here */}
+      <img 
+        src="https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://newprocesslab.com/wp-content/uploads/2021/12/cropped-Logo_NewProcessLab_60x523-1-1.png" 
+        alt="Logo" 
+        className="login-logo"
+      />
       <h2>Login</h2>
       <form onSubmit={handleLogin} className="login-form">
         <input
